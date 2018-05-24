67e3bdd642acda927b83aad47331b9a1c23e4704
COBRA0318 <ryo_chan@d5.dion.ne.jp>
2018-05-24 20:16:35 +0900

    7th Commit リスト4-18 を実現P128

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -22,13 +22,14 @@
         <a href="/register">登録</a>）</p>
     @endif -->
     @if(count($errors) > 0)
-    <div>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    </div>
+    <p>入力に問題があります。再入力して下さい。</p>
+    {{--<div>--}}
+        {{--<ul>--}}
+            {{--@foreach($errors->all() as $error)--}}
+                {{--<li>{{$error}}</li>--}}
+            {{--@endforeach--}}
+        {{--</ul>--}}
+    {{--</div>--}}
     @endif
     {{--   <table>
        <tr>
@@ -46,8 +47,17 @@
     <p>{{$msg}}</p>
     <table>
         <form action="/hello" method="post">
+            @if ($errors->has('name'))
+            <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+            @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+            @if ($errors->has('mail'))
+                <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+            @endif
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+            @if ($errors->has('mail'))
+                <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+            @endif
             <tr><th>age:  </th><td><input type="text" name="age"  value="{{old('age')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </form>
