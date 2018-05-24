a9f3fff81adf23e28d718f1abae3423c3363aca7
COBRA0318 <ryo_chan@d5.dion.ne.jp>
2018-05-24 18:55:10 +0900

    6th Commit リスト4-17 を実現P126

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -46,12 +46,20 @@
     <p>{{$msg}}</p>
     <table>
         <form action="/hello" method="post">
-            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-            <tr><th>age:  </th><td><input type="text" name="age"></td></tr>
+            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+            <tr><th>age:  </th><td><input type="text" name="age"  value="{{old('age')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </form>
     </table>
+    {{--<table>--}}
+        {{--<form action="/hello" method="post">--}}
+            {{--<tr><th>name: </th><td><input type="text" name="name"></td></tr>--}}
+            {{--<tr><th>mail: </th><td><input type="text" name="mail"></td></tr>--}}
+            {{--<tr><th>age:  </th><td><input type="text" name="age"></td></tr>--}}
+            {{--<tr><th></th><td><input type="submit" value="send"></td></tr>--}}
+        {{--</form>--}}
+    {{--</table>--}}
    @endsection
 
    @section('footer')
